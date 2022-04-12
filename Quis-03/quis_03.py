e41b779d4c7e713beffdb401fec3ce729c67bf12
yanyan-permana <deyanyan6@gmail.com>
2022-04-12 17:12:36 +0700

    Finish quis 03

--- Quis-03/quis_03.py
+++ Quis-03/quis_03.py
@@ -2,7 +2,7 @@ import ascii_magic
 import argparse
 try:
     parser = argparse.ArgumentParser()
-    parser.add_argument('p', help='Masukan path gambarnya bro !', type=str)
+    parser.add_argument('p', help='Masukan path gambar !', type=str)
 
     arg = parser.parse_args()
     path = arg.p
@@ -10,12 +10,4 @@ try:
     my_art = ascii_magic.from_image_file(path)
     ascii_magic.to_terminal(my_art)
 except Exception as e:
-    print('Error Bos !', e)
-
-
-# import ascii_magic
-# try:
-#     my_art = ascii_magic.from_image_file('img/uchiha.jpg')
-#     ascii_magic.to_terminal(my_art)
-# except Exception as e:
-#     print('Error Bos !', e)
+    print('Error!', e)
